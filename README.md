dd15595ad62d4fd3d7d262867ad4de03f437f441
Jack <jack@toumey.io>
2019-05-14 00:58:54 -0700

    first deploy

--- README.md
+++ README.md
@@ -29,9 +29,29 @@
 - using `ingress-nginx` by `kubernetes` (github.com/kubernetes/ingress-nginx), not `kubernetes-ingress` by `nginx`
 - ran `travis encrypt-file service-account.json -r miloofcroton/full-stack-k8s` to encrypt gcloud secret
 - had to fix ~/./travis/config.yml as follows (note the last line, it was `.org`):
-  ```
+    ```
     repos:
     miloofcroton/full-stack-k8s:
         endpoint: https://api.travis-ci.com/
-  ```
+    ```
 - as part of the above fix, I had to re-login using `travis login --pro` instead of just `travis login`
+- ran this in the gcloud shell to set context before creating secret
+    ```
+    gcloud config set project full-stack-k8s
+    gcloud config set compute/zone us-west1-a
+    gcloud container clusters get-credentials k8s-cluster
+    ```
+- ran this in the gcloud shell to generate actual secret (with actual password)
+    ```
+    kubectl create secret generic pgpassword --from-literal PGPASSWORD=<password>
+    ```
+- installed helm and tiller via https://helm.sh/docs/using_helm/#installing-helm in the gcloud console
+- create service account and cluster role binding, then assign cluster role binding to service account
+
+    ```
+    kubectl create serviceaccount --namespace kube-system tiller
+    kubectl create clusterrolebinding tiller-cluster-role --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+    ```
+
+- run `helm init --service-account tiller --upgrade` in gcloud console
+- run `helm install stable/nginx-ingress --name my-nginx --set rbac.create=true` in gcloud console
---
+++ deploy.sh
@@ -0,0 +1,25 @@
+docker pull miloofcroton/full-stack-docker-client:latest
+docker pull miloofcroton/full-stack-docker-server:latest
+docker pull miloofcroton/full-stack-docker-worker:latest
+
+docker build --cache-from miloofcroton/full-stack-docker-client -t miloofcroton/full-stack-docker-client:latest -f ./client/Dockerfile ./client
+docker build --cache-from miloofcroton/full-stack-docker-server -t miloofcroton/full-stack-docker-server:latest -f ./server/Dockerfile ./server
+docker build --cache-from miloofcroton/full-stack-docker-worker -t miloofcroton/full-stack-docker-worker:latest -f ./worker/Dockerfile ./worker
+
+docker tag miloofcroton/full-stack-docker-client:latest miloofcroton/full-stack-docker-client:$SHA
+docker tag miloofcroton/full-stack-docker-server:latest miloofcroton/full-stack-docker-server:$SHA
+docker tag miloofcroton/full-stack-docker-worker:latest miloofcroton/full-stack-docker-worker:$SHA
+
+docker push miloofcroton/full-stack-docker-client:latest
+docker push miloofcroton/full-stack-docker-server:latest
+docker push miloofcroton/full-stack-docker-worker:latest
+
+docker push miloofcroton/full-stack-docker-client:$SHA
+docker push miloofcroton/full-stack-docker-server:$SHA
+docker push miloofcroton/full-stack-docker-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=miloofcroton/full-stack-docker-server:$SHA --record
+kubectl set image deployments/client-deployment client=miloofcroton/full-stack-docker-client:$SHA --record
+kubectl set image deployments/worker-deployment worker=miloofcroton/full-stack-docker-worker:$SHA --record
