13a898202cc708d622ba586b02cdd59c8a83b47c
Jack <jack@toumey.io>
2019-05-14 17:14:23 -0700

    adding logging to server

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const keys = require('./keys');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('common'));
 app.use(cors());
 app.use(bodyParser.json());
 
