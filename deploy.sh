3c772f95a9e1ec665e1dfef06886a5bc26786e2d
Jack <jack@toumey.io>
2019-05-16 23:53:09 -0700

    better naming

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,29 @@
 # docker pull miloofcroton/full-stack-docker-client:latest
-# docker pull miloofcroton/full-stack-docker-server:latest
+# docker pull miloofcroton/full-stack-docker-api:latest
 # docker pull miloofcroton/full-stack-docker-worker:latest
 
 # docker build --cache-from miloofcroton/full-stack-docker-client -t miloofcroton/full-stack-docker-client:latest -f ./client/Dockerfile ./client
-# docker build --cache-from miloofcroton/full-stack-docker-server -t miloofcroton/full-stack-docker-server:latest -f ./server/Dockerfile ./server
+# docker build --cache-from miloofcroton/full-stack-docker-api -t miloofcroton/full-stack-docker-api:latest -f ./api/Dockerfile ./api
 # docker build --cache-from miloofcroton/full-stack-docker-worker -t miloofcroton/full-stack-docker-worker:latest -f ./worker/Dockerfile ./worker
 
 # docker tag miloofcroton/full-stack-docker-client:latest miloofcroton/full-stack-docker-client:$SHA
-# docker tag miloofcroton/full-stack-docker-server:latest miloofcroton/full-stack-docker-server:$SHA
+# docker tag miloofcroton/full-stack-docker-api:latest miloofcroton/full-stack-docker-api:$SHA
 # docker tag miloofcroton/full-stack-docker-worker:latest miloofcroton/full-stack-docker-worker:$SHA
 
 docker build -t miloofcroton/full-stack-docker-client:latest -t miloofcroton/full-stack-docker-client:$SHA -f ./client/Dockerfile ./client
-docker build -t miloofcroton/full-stack-docker-server:latest -t miloofcroton/full-stack-docker-server:$SHA -f ./server/Dockerfile ./server
+docker build -t miloofcroton/full-stack-docker-api:latest -t miloofcroton/full-stack-docker-api:$SHA -f ./api/Dockerfile ./api
 docker build -t miloofcroton/full-stack-docker-worker:latest -t miloofcroton/full-stack-docker-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push miloofcroton/full-stack-docker-client:latest
-docker push miloofcroton/full-stack-docker-server:latest
+docker push miloofcroton/full-stack-docker-api:latest
 docker push miloofcroton/full-stack-docker-worker:latest
 
 docker push miloofcroton/full-stack-docker-client:$SHA
-docker push miloofcroton/full-stack-docker-server:$SHA
+docker push miloofcroton/full-stack-docker-api:$SHA
 docker push miloofcroton/full-stack-docker-worker:$SHA
 
-kubectl apply -f k8s
+kubectl apply -f cluster
 
-kubectl set image deployments/client-deployment client=miloofcroton/full-stack-docker-client:$SHA --record
-kubectl set image deployments/server-deployment server=miloofcroton/full-stack-docker-server:$SHA --record
-kubectl set image deployments/worker-deployment worker=miloofcroton/full-stack-docker-worker:$SHA --record
+kubectl set image cluster/client-deployment client=miloofcroton/full-stack-docker-client:$SHA --record
+kubectl set image cluster/api-deployment api=miloofcroton/full-stack-docker-api:$SHA --record
+kubectl set image cluster/worker-deployment worker=miloofcroton/full-stack-docker-worker:$SHA --record
